fafe675d0ce518f2ba0bbe3650a0f944bbc625c6
Martin Růžek <me@martinruzek.cz>
2025-05-14 08:58:55 +0200

    improved code readability of the code and changed the favicon

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,7 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 
-// 2. Import loader(s)
 import { glob } from "astro/loaders";
 
-// 3. Define your collection(s)
 const projects = defineCollection({
   schema: ({ image }) =>
     z.object({
@@ -15,9 +12,7 @@ const projects = defineCollection({
       link: z.string(),
       order: z.number().optional(),
     }),
-  // Use the `glob` loader to load all files in the `src/content/projects` directory
   loader: glob({ pattern: ["**/*.md"], base: "src/content/projects" }),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
 export const collections = { projects };
